592fa1cd42029a9a281b6749a51d75d87c674e05
Ross Crawford-d'Heureuse <ross.crawford@adcloud.com>
2012-04-24 07:55:10 +0200

    Base setup

---
+++ site/pelican.conf.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+import os
+PROJECT_DIR = '/home/rossc/Projects/Personal/blog/site'
+
+AUTHOR = u"Ross Crawford-d'Heureuse"
+SITENAME = u"Ross Crawford-d'Heureuse - Django flavoured application development"
+SITEURL = '/'
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG='en'
+
+GITHUB_URL = 'http://github.com/stard0g101/'
+REVERSE_CATEGORY_ORDER = True
+
+# Blogroll
+LINKS =  (
+    ('Pelican', 'http://docs.notmyidea.org/alexis/pelican/'),
+    ('Python.org', 'http://python.org'),
+    ('Jinja2', 'http://jinja.pocoo.org'),
+    ('You can modify those links in your config file', '#')
+         )
+
+# Social widget
+SOCIAL = (
+          ('twitter', 'http://twitter.com/stardog101'),
+          ('github', 'http://twitter.com/stard0g101'),
+         )
+
+DEFAULT_PAGINATION = 5
+
+# global metadata to all the contents
+# DEFAULT_METADATA = (('yeah', 'it is'),)
+
+# static paths will be copied under the same name
+# STATIC_PATHS = ["pictures", ]
+
+# A list of files to copy from the source to the destination
+# FILES_TO_COPY = (('extra/robots.txt', 'robots.txt'),)
+
+THEME = os.path.join(PROJECT_DIR, '../pelican-themes/bootstrap2')
---
+++ site/src/start.md
@@ -0,0 +1,6 @@
+Date: 2010-12-03
+Title: A post from the blue
+Tags: thats, awesome
+Slug: a-post-from-the-blue
+
+Well, that was easy....
